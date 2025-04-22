77892706436bacf2d5a0c400015afdeeb74ac581
X E Qt R <61697058+Uotan@users.noreply.github.com>
2025-04-22 14:55:07 +0700

    Добавил проект на .Net 8
    Пытаюсь заставить работать это в среде Linux

---
+++ pkcs11interop_X/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Net.Pkcs11Interop.Common;
+using Net.Pkcs11Interop.HighLevelAPI;
+
+
+Console.WriteLine("type pkcs#11 full library path:");
+
+
+SetupCustomDllImportResolver();
+
+// Specify the path to unmanaged PKCS#11 library provided by the cryptographic device vendor
+string pkcs11LibraryPath = Console.ReadLine();
+
+// Create factories used by Pkcs11Interop library
+Pkcs11InteropFactories factories = new Pkcs11InteropFactories();
+
+// Load unmanaged PKCS#11 library
+using (IPkcs11Library pkcs11Library = factories.Pkcs11LibraryFactory.LoadPkcs11Library(factories, pkcs11LibraryPath, AppType.MultiThreaded))
+{
+    // Show general information about loaded library
+    ILibraryInfo libraryInfo = pkcs11Library.GetInfo();
+
+    Console.WriteLine("Library");
+    Console.WriteLine("  Manufacturer:       " + libraryInfo.ManufacturerId);
+    Console.WriteLine("  Description:        " + libraryInfo.LibraryDescription);
+    Console.WriteLine("  Version:            " + libraryInfo.LibraryVersion);
+
+    // Get list of all available slots
+    foreach (ISlot slot in pkcs11Library.GetSlotList(SlotsType.WithOrWithoutTokenPresent))
+    {
+        // Show basic information about slot
+        ISlotInfo slotInfo = slot.GetSlotInfo();
+
+        Console.WriteLine();
+        Console.WriteLine("Slot");
+        Console.WriteLine("  Manufacturer:       " + slotInfo.ManufacturerId);
+        Console.WriteLine("  Description:        " + slotInfo.SlotDescription);
+        Console.WriteLine("  Token present:      " + slotInfo.SlotFlags.TokenPresent);
+
+        if (slotInfo.SlotFlags.TokenPresent)
+        {
+            // Show basic information about token present in the slot
+            ITokenInfo tokenInfo = slot.GetTokenInfo();
+
+            Console.WriteLine("Token");
+            Console.WriteLine("  Manufacturer:       " + tokenInfo.ManufacturerId);
+            Console.WriteLine("  Model:              " + tokenInfo.Model);
+            Console.WriteLine("  Serial number:      " + tokenInfo.SerialNumber);
+            Console.WriteLine("  Label:              " + tokenInfo.Label);
+
+            // Show list of mechanisms (algorithms) supported by the token
+            Console.WriteLine("Supported mechanisms: ");
+            foreach (CKM mechanism in slot.GetMechanismList())
+                Console.WriteLine("  " + mechanism);
+        }
+    }
+}
+
+
+static void SetupCustomDllImportResolver()
+{
+#if NET5_0_OR_GREATER
+    if (Platform.IsLinux)
+    {
+        // Pkcs11Interop uses native functions from "libdl.so", but Ubuntu 22.04 and possibly also other distros have "libdl.so.2".
+        // Therefore, we need to set up a DllImportResolver to remap "libdl" to "libdl.so.2".
+        NativeLibrary.SetDllImportResolver(typeof(Pkcs11InteropFactories).Assembly, (libraryName, assembly, dllImportSearchPath) =>
+        {
+            if (libraryName == "libdl")
+            {
+                // Note: This mapping might need to be modified if your distribution uses a different version of libdl.
+                return NativeLibrary.Load("libdl.so.2", assembly, dllImportSearchPath);
+            }
+            else
+            {
+                return NativeLibrary.Load(libraryName, assembly, dllImportSearchPath);
+            }
+        });
+    }
+#endif
+}
--- pkcs11interop_testing/Program.cs
+++ pkcs11interop_testing/Program.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using Net.Pkcs11Interop.Common;
 using Net.Pkcs11Interop.HighLevelAPI;
-using Net.Pkcs11Interop.Logging;
-using Net.Pkcs11Interop.LowLevelAPI40;
 
 namespace pkcs11interop_testing
 {
@@ -16,37 +10,82 @@ namespace pkcs11interop_testing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter library path");
-            string libraryPath = Console.ReadLine();
+            // Set up a custom DllImportResolver that may be needed on some Linux distributions
+            SetupCustomDllImportResolver();
 
-            using (IPkcs11Library pkcs11Library = Settings.Factories.Pkcs11LibraryFactory.LoadPkcs11Library(Settings.Factories, libraryPath, Settings.AppType))
+            // Specify the path to unmanaged PKCS#11 library provided by the cryptographic device vendor
+            string pkcs11LibraryPath = @"c:\SoftHSM2\lib\softhsm2-x64.dll";
+
+            // Create factories used by Pkcs11Interop library
+            Pkcs11InteropFactories factories = new Pkcs11InteropFactories();
+
+            // Load unmanaged PKCS#11 library
+            using (IPkcs11Library pkcs11Library = factories.Pkcs11LibraryFactory.LoadPkcs11Library(factories, pkcs11LibraryPath, AppType.MultiThreaded))
             {
+                // Show general information about loaded library
                 ILibraryInfo libraryInfo = pkcs11Library.GetInfo();
-                Console.WriteLine(libraryInfo.LibraryVersion);
-                Console.WriteLine(libraryInfo.ManufacturerId);
-                Console.WriteLine(libraryInfo.CryptokiVersion);
-                Console.WriteLine(libraryInfo.Flags);
-                
-            }
 
+                Console.WriteLine("Library");
+                Console.WriteLine("  Manufacturer:       " + libraryInfo.ManufacturerId);
+                Console.WriteLine("  Description:        " + libraryInfo.LibraryDescription);
+                Console.WriteLine("  Version:            " + libraryInfo.LibraryVersion);
 
-            using (IPkcs11Library pkcs11Library = Settings.Factories.Pkcs11LibraryFactory.LoadPkcs11Library(Settings.Factories, libraryPath, Settings.AppType))
-            {
-                // Get list of available slots
-                List<ISlot> slots = pkcs11Library.GetSlotList(SlotsType.WithTokenPresent);
-
-                foreach (ISlot slot in slots)
+                // Get list of all available slots
+                foreach (ISlot slot in pkcs11Library.GetSlotList(SlotsType.WithOrWithoutTokenPresent))
                 {
-                    var slotInfo = slot.GetSlotInfo();
-                    //Debug.WriteLine(slotInfo.ManufacturerId);
-                    Console.WriteLine(slotInfo.SlotId);
-                    var token = slot.GetTokenInfo();
-                    Console.WriteLine(token.SerialNumber);
-                    Console.WriteLine(token.Label);
-                    Console.WriteLine(token.Model);
-                    Console.WriteLine("__________________________");
+                    // Show basic information about slot
+                    ISlotInfo slotInfo = slot.GetSlotInfo();
+
+                    Console.WriteLine();
+                    Console.WriteLine("Slot");
+                    Console.WriteLine("  Manufacturer:       " + slotInfo.ManufacturerId);
+                    Console.WriteLine("  Description:        " + slotInfo.SlotDescription);
+                    Console.WriteLine("  Token present:      " + slotInfo.SlotFlags.TokenPresent);
+
+                    if (slotInfo.SlotFlags.TokenPresent)
+                    {
+                        // Show basic information about token present in the slot
+                        ITokenInfo tokenInfo = slot.GetTokenInfo();
+
+                        Console.WriteLine("Token");
+                        Console.WriteLine("  Manufacturer:       " + tokenInfo.ManufacturerId);
+                        Console.WriteLine("  Model:              " + tokenInfo.Model);
+                        Console.WriteLine("  Serial number:      " + tokenInfo.SerialNumber);
+                        Console.WriteLine("  Label:              " + tokenInfo.Label);
+
+                        // Show list of mechanisms (algorithms) supported by the token
+                        Console.WriteLine("Supported mechanisms: ");
+                        foreach (CKM mechanism in slot.GetMechanismList())
+                            Console.WriteLine("  " + mechanism);
+                    }
                 }
             }
+        }
+
+        /// <summary>
+        /// Sets up a custom DllImportResolver that may be needed when Pkcs11Interop is running on Linux with .NET 5 or later
+        /// </summary>
+        static void SetupCustomDllImportResolver()
+        {
+
+            if (Platform.IsLinux)
+            {
+                // Pkcs11Interop uses native functions from "libdl.so", but Ubuntu 22.04 and possibly also other distros have "libdl.so.2".
+                // Therefore, we need to set up a DllImportResolver to remap "libdl" to "libdl.so.2".
+                NativeLibrary.SetDllImportResolver(typeof(Pkcs11InteropFactories).Assembly, (libraryName, assembly, dllImportSearchPath) =>
+                {
+                    if (libraryName == "libdl")
+                    {
+                        // Note: This mapping might need to be modified if your distribution uses a different version of libdl.
+                        return NativeLibrary.Load("libdl.so.2", assembly, dllImportSearchPath);
+                    }
+                    else
+                    {
+                        return NativeLibrary.Load(libraryName, assembly, dllImportSearchPath);
+                    }
+                });
+            }
+
         }
     }
 }
