ad67bd7535985138debbaa4497031bd978e7b26f
Uotan Heinz <rezer.new@mail.ru>
2025-05-03 19:16:04 +0700

    test: Протестировал чтение списка объектов и получение их значений

--- pkcs11interop_testing/Program.cs
+++ pkcs11interop_testing/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Runtime;
 using System.Runtime.InteropServices;
+using System.Text;
 using Net.Pkcs11Interop.Common;
 using Net.Pkcs11Interop.HighLevelAPI;
 
@@ -15,45 +16,131 @@ namespace pkcs11interop_testing
         {
             Pkcs11InteropFactories pkcs11InteropFactories = new Pkcs11InteropFactories();
             string libraryPath = @"C:\Windows\System32\jcPKCS11-2.dll";
-            List<ISlot> slots = null;
             string userPIN = "11111111";
             string testLabel = "test label";
             string testData = "test DATA";
 
-            CreateDestroyObjectTest();
+            List<ISlot> slots = null;
+            ISlot slot = null;
+
+
+
+            LessInitThisThing();
+
+            //CreateDestroyObjectTest();
+
+            //FindAllObjects();
+
+
+            string keyValue = null;
+            keyValue = FindKeyObject();
+            Console.WriteLine(keyValue);
+
+            Console.ReadKey();
+
+
+
+            void LessInitThisThing()
+            {
+                IPkcs11Library pkcs11Library = pkcs11InteropFactories.Pkcs11LibraryFactory.LoadPkcs11Library(pkcs11InteropFactories,
+                    libraryPath, AppType.MultiThreaded);
+                slots = pkcs11Library.GetSlotList(SlotsType.WithTokenPresent);
+                slot = slots.FirstOrDefault();
+            }
 
             void CreateDestroyObjectTest()
             {
-                using (IPkcs11Library pkcs11Library = pkcs11InteropFactories.Pkcs11LibraryFactory.LoadPkcs11Library(pkcs11InteropFactories, libraryPath, AppType.MultiThreaded))
+
+                // Open RW session
+                using (ISession session = slot.OpenSession(SessionType.ReadWrite))
                 {
-                    // Find first slot with token present
-                    slots = pkcs11Library.GetSlotList(SlotsType.WithTokenPresent);
+                    // Login as normal user
+                    session.Login(CKU.CKU_USER, userPIN);
 
-                    ISlot slot = slots.FirstOrDefault();
+                    // Prepare attribute template of new data object
+                    List<IObjectAttribute> objectAttributes = new List<IObjectAttribute>();
+                    objectAttributes.Add(session.Factories.ObjectAttributeFactory.Create(CKA.CKA_CLASS, CKO.CKO_DATA));
+                    objectAttributes.Add(session.Factories.ObjectAttributeFactory.Create(CKA.CKA_TOKEN, true));
+                    objectAttributes.Add(session.Factories.ObjectAttributeFactory.Create(CKA.CKA_PRIVATE, true));
+                    objectAttributes.Add(session.Factories.ObjectAttributeFactory.Create(CKA.CKA_MODIFIABLE, false));
+                    objectAttributes.Add(session.Factories.ObjectAttributeFactory.Create(CKA.CKA_LABEL, testLabel));
+                    objectAttributes.Add(session.Factories.ObjectAttributeFactory.Create(CKA.CKA_VALUE, testData));
 
-                    // Open RW session
-                    using (ISession session = slot.OpenSession(SessionType.ReadWrite))
-                    {
-                        // Login as normal user
-                        session.Login(CKU.CKU_USER, userPIN);
+                    // Create object
+                    IObjectHandle objectHandle = session.CreateObject(objectAttributes);
 
-                        // Prepare attribute template of new data object
-                        List<IObjectAttribute> objectAttributes = new List<IObjectAttribute>();
-                        objectAttributes.Add(session.Factories.ObjectAttributeFactory.Create(CKA.CKA_CLASS, CKO.CKO_DATA));
-                        objectAttributes.Add(session.Factories.ObjectAttributeFactory.Create(CKA.CKA_TOKEN, true));
-                        objectAttributes.Add(session.Factories.ObjectAttributeFactory.Create(CKA.CKA_PRIVATE, true));
-                        objectAttributes.Add(session.Factories.ObjectAttributeFactory.Create(CKA.CKA_MODIFIABLE, false));
-                        objectAttributes.Add(session.Factories.ObjectAttributeFactory.Create(CKA.CKA_LABEL, testLabel));
-                        objectAttributes.Add(session.Factories.ObjectAttributeFactory.Create(CKA.CKA_VALUE, testData));
+                    // Do something interesting with new object
 
-                        // Create object
-                        IObjectHandle objectHandle = session.CreateObject(objectAttributes);
-
-                        // Do something interesting with new object
-
-                        session.Logout();
-                    }
+                    session.Logout();
                 }
+
+            }
+
+            void FindAllObjects()
+            {
+
+                using (ISession session = slot.OpenSession(SessionType.ReadOnly))
+                {
+                    // Login as normal user
+                    session.Login(CKU.CKU_USER, userPIN);
+
+                    List<IObjectAttribute> searchTemplate = new List<IObjectAttribute>();
+                    searchTemplate.Add(session.Factories.ObjectAttributeFactory.Create(CKA.CKA_CLASS, CKO.CKO_DATA));
+
+                    List<IObjectHandle> foundObjects = session.FindAllObjects(searchTemplate);
+                    foreach (var foundObject in foundObjects)
+                    {
+                        List<ulong> attributes = new List<ulong>();
+                        attributes.Add((ulong)CKA.CKA_PRIVATE);
+                        attributes.Add((ulong)CKA.CKA_LABEL);
+                        attributes.Add((ulong)CKA.CKA_APPLICATION);
+                        attributes.Add((ulong)CKA.CKA_VALUE);
+
+                        List<IObjectAttribute> requiredAttributes = session.GetAttributeValue(foundObject, attributes);
+                        Console.Write(requiredAttributes[1].GetValueAsString()+"\t");
+                        Console.WriteLine(Encoding.UTF8.GetString(requiredAttributes[3].GetValueAsByteArray()));
+                    }
+
+                    session.Logout();
+                }
+
+            }
+
+
+            string FindKeyObject()
+            {
+                string _keyValue = null;
+                using (ISession session = slot.OpenSession(SessionType.ReadOnly))
+                {
+                    // Login as normal user
+                    session.Login(CKU.CKU_USER, userPIN);
+
+                    List<IObjectAttribute> searchTemplate = new List<IObjectAttribute>();
+                    searchTemplate.Add(session.Factories.ObjectAttributeFactory.Create(CKA.CKA_CLASS, CKO.CKO_DATA));
+
+                    List<IObjectHandle> foundObjects = session.FindAllObjects(searchTemplate);
+                    foreach (var foundObject in foundObjects)
+                    {
+                        List<ulong> attributes = new List<ulong>();
+                        attributes.Add((ulong)CKA.CKA_PRIVATE);
+                        attributes.Add((ulong)CKA.CKA_LABEL);
+                        attributes.Add((ulong)CKA.CKA_APPLICATION);
+                        attributes.Add((ulong)CKA.CKA_VALUE);
+
+                        List<IObjectAttribute> requiredAttributes = session.GetAttributeValue(foundObject, attributes);
+                        if (requiredAttributes[1].GetValueAsString().Contains("key"))
+                        {
+                            _keyValue = Encoding.UTF8.GetString(requiredAttributes[3].GetValueAsByteArray());
+                        }
+                        break;
+                    }
+
+                    session.Logout();
+
+                    
+                }
+                return _keyValue;
+
             }
 
         }
