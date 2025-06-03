be86684384c0e406a0e0fcdda208795bd3fcc294
X E Qt R <61697058+Uotan@users.noreply.github.com>
2025-06-03 19:20:29 +0700

    refactor: немного причесал код, переименовал весь проект в изначальное название

---
+++ KeePKCS11/Forms/FormEnterPIN.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace KeePKCS11.Forms
+{
+    partial class FormEnterPIN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxPinCode = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.checkBoxDisplayPIN = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Enter your PIN:";
+            // 
+            // tbxPinCode
+            // 
+            this.tbxPinCode.Location = new System.Drawing.Point(15, 39);
+            this.tbxPinCode.Name = "tbxPinCode";
+            this.tbxPinCode.Size = new System.Drawing.Size(269, 20);
+            this.tbxPinCode.TabIndex = 0;
+            this.tbxPinCode.UseSystemPasswordChar = true;
+            this.tbxPinCode.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbxPinCode_KeyDown);
+            // 
+            // btnOk
+            // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Location = new System.Drawing.Point(125, 93);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 12;
+            this.btnOk.Text = "&OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(206, 93);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 11;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // checkBoxDisplayPIN
+            // 
+            this.checkBoxDisplayPIN.AutoSize = true;
+            this.checkBoxDisplayPIN.Location = new System.Drawing.Point(15, 66);
+            this.checkBoxDisplayPIN.Name = "checkBoxDisplayPIN";
+            this.checkBoxDisplayPIN.Size = new System.Drawing.Size(81, 17);
+            this.checkBoxDisplayPIN.TabIndex = 15;
+            this.checkBoxDisplayPIN.Text = "Display PIN";
+            this.checkBoxDisplayPIN.UseVisualStyleBackColor = true;
+            this.checkBoxDisplayPIN.CheckedChanged += new System.EventHandler(this.checkBoxDisplayPIN_CheckedChanged);
+            // 
+            // FormEnterPIN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 138);
+            this.Controls.Add(this.checkBoxDisplayPIN);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxPinCode);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnCancel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormEnterPIN";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Enter PIN code";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxPinCode;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.CheckBox checkBoxDisplayPIN;
+    }
+}
--- build_plgx.cmd
+++ build_plgx.cmd
@@ -6,7 +6,7 @@ echo Current directory is: %CURRENT_DIR%
 
 cd KeePass-2.58
 
-KeePass.exe --plgx-create %CURRENT_DIR%KeePKCS11KeyProvider
+KeePass.exe --plgx-create %CURRENT_DIR%KeePKCS11
 
 echo KeePKCS11.plgx was created in %CURRENT_DIR%
 pause
