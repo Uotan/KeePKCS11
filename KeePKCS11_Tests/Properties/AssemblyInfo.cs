eec9731bb4581008acad02bad4b2f3bc8e238434
X E Qt R <61697058+Uotan@users.noreply.github.com>
2025-06-09 08:23:27 +0700

    chore: добавлен проект модульного тестирования

---
+++ KeePKCS11_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KeePKCS11_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KeePKCS11_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("54895575-1f3c-4ae2-bdca-9fed05cf2723")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ KeePKCS11_Tests/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Security.Cryptography;
+
+namespace KeePKCS11_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string GenerateRandomPassword()
+            {
+                int length = 32;
+                string allowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                if (length < 1)
+                    throw new ArgumentException("Длина пароля должна быть положительной", nameof(length));
+
+                if (string.IsNullOrEmpty(allowedChars))
+                    throw new ArgumentException("Набор символов не может быть пустым", nameof(allowedChars));
+
+                char[] password = new char[length];
+                byte[] randomBytes = new byte[length * 4]; // Буфер для случайных байт
+
+                using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+                {
+                    rng.GetBytes(randomBytes); // Заполняем буфер криптостойкими байтами
+
+                    for (int i = 0; i < length; i++)
+                    {
+                        // Преобразуем случайные байты в индекс символа
+                        uint randomValue = BitConverter.ToUInt32(randomBytes, i * 4);
+                        password[i] = allowedChars[(int)(randomValue % (uint)allowedChars.Length)];
+                    }
+                }
+
+                return new string(password);
+            }
+            string myStrongPasswor = GenerateRandomPassword();
+            Console.WriteLine(myStrongPasswor);
+        }
+    }
+}
